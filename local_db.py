1da39ce5d24d16316576cd7a17eab7ef950b7f39
Josue Lopez <josueal1@uci.edu>
2019-12-31 06:44:38 -0800

    feat(base): update navbar, 404, contact

--- local_db.py
+++ local_db.py
@@ -14,37 +14,18 @@ SOCIAL_LINKS = [Link("LinkedIn","https://www.linkedin.com/in/josue-a-lopez/"),
 				Link("Twitter", "https://www.twitter.com/galvez2j")
 		 	   ]
 
-NAV_LINKS = [Link("About","/about"),
-			 Link("Contact","#contact"),
-			 Link("Recommendations","#recommendations"),
-			 Link("iPhoneography Portfolio","/portfolio"),
-			 Link("Services","/services"),
-			 Link("Blog", "/blog")
+NAV_LINKS = [Link("About","/"),
+			 Link("Blog", "/blog"),
+			 Link("Timeline","/Timeline"),
+			 Link("Gallery","/portfolio"),
+			 Link("Contact","/contact"),
 			]
-
-SERVICES = [
-	{ "icon": "perm_contact_calendar",
-	  "title":"Web-Development Consultations",
-	  "message":"""Have you been wanting to build that website for your small business, software project, or for your CS portfolio? Then let's talk! Throughout my two years, I have specialized in Python and JavaScript web development!""",
-	  "href":"/contact",
-	  "link_title":"Schedule a Consultation"
-	},
-	{ "icon" :"developer_mode",
-	  "title":"Python, C++, Java Tutorials",
-	  "message":"You can spend a millions hours finding the best course of action, but I have curated a list of top three programming YouTube playlist that have helped me succeed as a software developer at UCI!",
-	  "href":"/recommendations",
-	  "link_title":"Check Them Out!"
-	}
-]
-
 
 BLOG_POSTS = [	{"title": "My First Blog Post", "content": "Because of the proliferation of personal-branding, as sophomore undergrad, I've decided to take-on the world with a keyboard and a screen (plus some wifi). Through this blog, I intend to share life goals, entrepreneural ambitions, and wild visions of tech."}, 
 				{"title": "My Second Blog Post", "content": "This is just a test."}
 			 ]
 
-
 VID_RECOMMENDATIONS = ["PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU","PLAE85DE8440AA6B83","PL27BCE863B6A864E3", "PL4cUxeGkcC9gGrbtvASEZSlFEYBnPkmff"]
-
 
 LIFE_EVENTS = [ Event("April 2017", "Confirmed offer letter from UC Irvine! Zot, Zot! :emoji: "),
 					Event("July 2017","Flew out to Seattle, WA for Google's Computer Science Summer Institute!"),
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 from datetime import datetime
-from local_db import SOCIAL_LINKS, SERVICES, BLOG_POSTS, VID_RECOMMENDATIONS, NAV_LINKS, LIFE_EVENTS
+from local_db import SOCIAL_LINKS, BLOG_POSTS, VID_RECOMMENDATIONS, NAV_LINKS, LIFE_EVENTS
 
 app = Flask(__name__)
 
@@ -9,17 +9,15 @@ app.config['SECRET_KEY'] = '2e9cddbf90516b76b574b4b2411b43a0'
 @app.context_processor
 def utility_processor():
 	'''The items in the return dict are available within all the templates in the app.'''
-	return dict(footer_date=datetime.now(), timeline_events=LIFE_EVENTS)
-	
+	return dict(footer_date=datetime.now(), timeline_events=LIFE_EVENTS, NAV_LINKS=NAV_LINKS)
 
 @app.route("/")
-@app.route("/about")
 def about():
 	return render_template("home.html", page_title="About", SOCIAL_LINKS=SOCIAL_LINKS, NAV_LINKS=NAV_LINKS)
 
-@app.route("/services")
-def services():
-	return render_template("services.html", page_title="Services", SERVICES=SERVICES)
+@app.route("/contact")
+def contact():
+	return render_template("contact.html", page_title="Contact | Services")
 
 @app.route("/blog")
 def blog():
@@ -33,6 +31,10 @@ def recommendations():
 def portfolio():
 	return render_template("portfolio.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 40
 
 if __name__ == "__main__":
 	app.run(debug=True)
